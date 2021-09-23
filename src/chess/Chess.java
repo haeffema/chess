eef7b8f3cd6b8596c161e747c522e055b3526255
haeffema <haeffema@gmail.com>
2021-09-23 12:44:22 +0200

    anfangen board zu strukturieren

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,5 +1,13 @@
 package chess;
 
 public class Chess {
-	Figure[][] board = new Figure[8][8];
+    Figure[][] board = {{new Figure("T", 0, 0, true), new Figure("P", 1, 0, true), new Figure("L", 0, 2, true), new Figure("D", 0, 3, true), new Figure("K", 0, 4, true), new Figure("L", 0, 5, true), new Figure("F", 0, 6, true), new Figure("T", 0, 7, true)},
+            {new Figure("B", 1, 0, true), new Figure("B", 1, 1, true), new Figure("B", 1, 2, true), new Figure("B", 1, 3, true), new Figure("B", 1, 4, true), new Figure("B", 1, 5, true), new Figure("B", 1, 6, true), new Figure("B", 1, 7, true)},
+            {},
+            {},
+            {},
+            {},
+            {},
+            {}};
+
 }
