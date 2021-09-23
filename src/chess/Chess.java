b1e18c279fc4cc6aedc307dd84a331ed43b58ebd
haeffema <haeffema@gmail.com>
2021-09-23 13:51:54 +0200

    grund basis steht :)

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,13 +1,26 @@
 package chess;
 
 public class Chess {
-    Figure[][] board = {{new Figure("T", 0, 0, true), new Figure("P", 1, 0, true), new Figure("L", 0, 2, true), new Figure("D", 0, 3, true), new Figure("K", 0, 4, true), new Figure("L", 0, 5, true), new Figure("F", 0, 6, true), new Figure("T", 0, 7, true)},
+    static Figure[][] board = {{new Figure("T", 0, 0, true), new Figure("P", 1, 0, true), new Figure("L", 0, 2, true), new Figure("D", 0, 3, true), new Figure("K", 0, 4, true), new Figure("L", 0, 5, true), new Figure("F", 0, 6, true), new Figure("T", 0, 7, true)},
             {new Figure("B", 1, 0, true), new Figure("B", 1, 1, true), new Figure("B", 1, 2, true), new Figure("B", 1, 3, true), new Figure("B", 1, 4, true), new Figure("B", 1, 5, true), new Figure("B", 1, 6, true), new Figure("B", 1, 7, true)},
-            {},
-            {},
-            {}, // Hier das Schachfeld halt weiter einfügen :)
-            {},
-            {},
-            {}};
+            {null, null, null, null, null, null, null, null},
+            {null, null, null, null, null, null, null, null},
+            {null, null, null, null, null, null, null, null}, // Hier das Schachfeld halt weiter einfügen :)
+            {null, null, null, null, null, null, null, null},
+            {new Figure("B", 6, 0, false), new Figure("B", 6, 1, false), new Figure("B", 6, 2, false), new Figure("B", 6, 3, false), new Figure("B", 6, 4, false), new Figure("B", 6, 5, false), new Figure("B", 6, 6, false), new Figure("B", 6, 7, false)},
+            {new Figure("T", 7, 0, true), new Figure("P", 7, 0, true), new Figure("L", 7, 2, true), new Figure("D", 7, 3, true), new Figure("K", 7, 4, true), new Figure("L", 7, 5, true), new Figure("F", 7, 6, true), new Figure("T", 7, 7, true)}};
 
+    public static void print_board() { // Des wird noch besser, ist nur zum testen ob das game so klappt :)
+        for (int i = 0; i < 8; i++) {
+            for (int k = 0; k < 8; k++) {
+                if (board[i][k] != null) {
+                    System.out.print(board[i][k].name);
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
 }
--- src/chess/Main.java
+++ src/chess/Main.java
@@ -3,6 +3,7 @@ package chess;
 public class Main {
 
 	public static void main(String[] args) {
+		Chess.print_board();
 	}
 
 }
